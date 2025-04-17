9871480401ba9fe43b31c72b3b70677b464e5e3f
Manish Mathai <manishpmathai@gmail.com>
2025-04-17 13:58:34 -0700

    Fixes and enhances original AFGSA model
    A variety of fixes and modifications were made to have AFGSA model run
    on volumetric path traced images:
    - Fixed get_cropped_patches function calls to remove extra parameter
      that causes errors
    - Fixed uses of numpy.inf
    - Made noisy and ground truth image spp values configurable. This make
      it HDF5Constructor work for our data sets
    - Updated uses of fixed value of 128 for patch size to use a variable
    instead
    - Removed surface based aux buffers, keeping just "noisy", "gt" and "aux"
      while extracting patches or when persisting them to disk.
    - Updated the discriminator to use an adaptive approach for calculating
      convolutional layer parameter count instead of using fixed values.
      This was necessary to make it work for patch sizes other than 128.

---
+++ pht/models/afgsa/code/wo_diff_spec_decomp/train/gen_hdf5.py
@@ -0,0 +1,120 @@
+from preprocessing import *
+import os
+import random
+import numpy as np
+import h5py
+from multiprocessing import Queue, Process, Value
+import multiprocessing
+
+
+class Hdf5Constructor:
+    def __init__(self, data_path, save_path, patch_size, num_patches, seed, train_val_ratio, noisy_spp=32, gt_spp=1024):
+        assert sum(train_val_ratio) == 1, "Sum of train_val_ratio must be 1!"
+        self.data_path = data_path
+        self.save_path = save_path
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.seed = seed
+        self.train_val_ratio = train_val_ratio
+        self.noisy_spp = noisy_spp
+        self.gt_spp = gt_spp
+
+        self.exr_paths = []
+        self.gt_paths = []
+        self.paths = []
+
+    def construct_hdf5(self):
+        print("Constructing data set (hdf5)")
+        self.get_exr_paths()
+        self.get_cropped_patches()
+        print("Constructing data set (hdf5): done")
+
+    def get_exr_paths(self):
+        print("\r\t-Get exr paths", end="")
+        self.noisy_path = os.path.join(self.data_path, f"{self.noisy_spp}spp")
+        self.gt_path = os.path.join(self.data_path, f"{self.gt_spp}spp")
+        for root, dirs, files in os.walk(self.gt_path):
+            if not dirs:
+                for file in files:
+                    gt_path = os.path.join(
+                        self.gt_path,
+                        f"{file.split("_")[0]}_{file.split("_")[1]}_{self.gt_spp}"
+                    )
+                    exr_path = os.path.join(
+                        self.noisy_path,
+                        f"{file.split("_")[0]}_{file.split("_")[1]}_{self.noisy_spp}"
+                    )
+                    if gt_path not in self.gt_paths:
+                        self.gt_paths.append(gt_path)
+                        self.exr_paths.append(exr_path)
+        assert len(self.exr_paths) == len(self.gt_paths), "#samples does not equal to #gts, check the data!"
+
+        assert type(self.seed) is int, "seed must be an int!"
+        random.seed(self.seed)
+        self.paths = [(e, g) for e, g in zip(self.exr_paths, self.gt_paths)]
+        random.shuffle(self.paths)
+        print("\r\t-Get exr paths: done", end='')
+
+
+    def worker(self, queues, path_mapping, name_shape_mapping, lock, v):
+        while not queues[0].empty() or not queues[1].empty():
+            v.value += 1
+            print("\r\t-Generating patches: %d / %d" % (v.value, len(self.paths)-3), end='')
+            if not queues[0].empty():
+                path = queues[0].get()
+                dataset = 'train'
+            elif not queues[1].empty():
+                path = queues[1].get()
+                dataset = 'val'
+            cropped, patches = get_cropped_patches(path[0], path[1], self.patch_size, self.num_patches)
+
+            lock.acquire()
+            with h5py.File(path_mapping[dataset], 'a') as hf:
+                for key in name_shape_mapping.keys():
+                    temp = [c[key] for c in cropped]
+                    hf[key].resize((hf[key].shape[0] + len(temp)), axis=0)
+                    hf[key][-len(temp):] = temp
+            lock.release()
+
+
+    def get_cropped_patches(self):
+        patch_count = 0
+        train_save_path = os.path.join(self.save_path, "train.h5")
+        val_save_path = os.path.join(self.save_path, "val.h5")
+        path_mapping = {'train': train_save_path, 'val': val_save_path}
+        name_shape_mapping = {'noisy': (None, self.patch_size, self.patch_size, 3),
+                              'gt': (None, self.patch_size, self.patch_size, 3),
+                              'aux': (None, self.patch_size, self.patch_size, 7)}
+        queues = [Queue() for i in range(2)]  # [train_queue, val_queue]
+        # the first 2 paths are used to initiate h5py files
+        for i in range(2, len(self.paths)):
+            if (i - 2) < int(self.train_val_ratio[0] * (len(self.paths) - 2)):
+                queues[0].put(self.paths[i])
+            else:
+                queues[1].put(self.paths[i])
+        # initiate h5py files
+        print("\n\r\t-Initiating h5py files", end='')
+        for i, n in enumerate(['train', 'val']):
+            with h5py.File(path_mapping[n], 'w') as hf:
+                cropped, patches = get_cropped_patches(self.paths[i][0], self.paths[i][1], self.patch_size,
+                                                       self.num_patches)
+                patch_count += len(cropped)
+                for key in name_shape_mapping.keys():
+                    temp = np.array([c[key] for c in cropped])
+                    hf.create_dataset(key, data=temp, maxshape=name_shape_mapping[key], compression="gzip",
+                                      chunks=True)
+        print("\r\t-Initiating h5py files: done")
+
+        # start processes
+        lock = multiprocessing.Lock()  # to ensure only one process writes to the file at once
+        done_exr_count = Value("i", 0)
+        pool = [Process(target=self.worker, args=(queues, path_mapping, name_shape_mapping, lock, done_exr_count))
+                for i in range(multiprocessing.cpu_count() - 1)]
+        for p in pool:
+            p.start()
+        for p in pool:
+            p.join()
+
+        print("\r\t-Generating patches: done")
+
+
---
+++ train.sh
@@ -0,0 +1,197 @@
+#!//bin/bash
+# Enable strict error handling:
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error and exit immediately.
+# -o pipefail: Return the exit status of the last command in the pipeline that failed.
+set -euo pipefail
+
+if [[ -f ".venv/bin/activate" ]]; then
+    # shellcheck disable=SC1091
+    source .venv/bin/activate
+else
+    echo "Virtual‑env not found (.venv/bin/activate). Aborting." >&2
+    exit 1
+fi
+
+# ---------------- Default values -----------------
+DATA_MODE=""
+NUM_EPOCHS=0
+CLI_NUM_EPOCHS=0
+USE_PATCHES=true
+NUM_PATCHES=0
+PATCH_SIZE=0
+CLI_PATCH_SIZE=0
+LOG_FILE="run_log.txt"
+INTERACTIVE=false
+APP_MODE="afgsa"
+
+cleanup() {
+    echo "Process interrupted. Cleaning up..."
+    exit 130
+}
+trap cleanup INT
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -s | --small)
+        DATA_MODE="small"
+        shift
+        ;;
+    -f | --full)
+        DATA_MODE="full"
+        shift
+        ;;
+    -l | --limited)
+        DATA_MODE="limited"
+        shift
+        ;;
+    -i | --interactive)
+        INTERACTIVE=true
+        shift
+        ;;
+    -a | --afgsa)
+        APP_MODE="afgsa"
+        shift
+        ;;
+    -m | --mamba)
+        APP_MODE="mamba"
+        shift
+        ;;
+    -e | --epochs)
+        CLI_NUM_EPOCHS="$2"
+        shift 2
+        ;;
+    -P | --no-patches)
+        USE_PATCHES=false
+        shift
+        ;;
+    --patch-size)
+        CLI_PATCH_SIZE="$2"
+        shift 2
+        ;;
+    *)
+        echo "Unknown option: $1" >&2
+        exit 1
+        ;;
+    esac
+done
+
+if [[ -z "$DATA_MODE" ]]; then
+    echo "Please specify the dataset mode: -s|--small, -f|--full, -l|--limited" >&2
+    exit 1
+fi
+
+BASE_DIR="${HOME}/projects/pixel_heal_thyself"
+INPUT_DIR="${BASE_DIR}/data/images_${DATA_MODE}"
+RUN_ROOT="${BASE_DIR}/runs/${APP_MODE}_${DATA_MODE}"
+mkdir -p "${RUN_ROOT}"
+
+get_next_run_directory() {
+    local run_root="$1"
+    local next_id=1
+
+    if [[ -d "${run_root}" ]]; then
+        # Check if any run directories exist
+        if compgen -G "${run_root}/run_*" >/dev/null; then
+            last=$(ls -d ${run_root}/run_* 2>/dev/null | sort -V | tail -n1)
+            if [[ -n "${last}" && "${last##*_}" =~ ^[0-9]+$ ]]; then
+                next_id=$((10#${last##*_} + 1))
+            else
+                next_id=1
+            fi
+        fi
+    else
+        echo "Run root directory does not exist. Exiting."
+        exit 1
+    fi
+
+    printf "%s/run_%03d" "${run_root}" "${next_id}"
+}
+
+OUTPUT_DIR="$(get_next_run_directory "${RUN_ROOT}")"
+mkdir -p "${OUTPUT_DIR}"
+: >"${OUTPUT_DIR}/${LOG_FILE}" # truncate/creates log file
+
+log() { printf '%s\n' "$*" >>"${OUTPUT_DIR}/${LOG_FILE}"; }
+
+case "$DATA_MODE" in
+full)
+    NUM_EPOCHS=12
+    NUM_PATCHES=400
+    PATCH_SIZE=128
+    ;;
+limited)
+    NUM_EPOCHS=12
+    NUM_PATCHES=200
+    PATCH_SIZE=64
+    ;;
+small)
+    NUM_EPOCHS=12
+    NUM_PATCHES=100
+    PATCH_SIZE=32
+    ;;
+esac
+
+# CLI overrides
+[[ $CLI_NUM_EPOCHS -gt 0 ]] && NUM_EPOCHS=$CLI_NUM_EPOCHS
+[[ $CLI_PATCH_SIZE -gt 0 ]] && PATCH_SIZE=$CLI_PATCH_SIZE
+
+SEPARATOR=$(printf '─%.0s' {1..100})
+log "Training Data:"
+log "$SEPARATOR"
+log "Base dir   : ${BASE_DIR}"
+log "Dataset    : ${DATA_MODE}"
+log "Input dir  : ${INPUT_DIR}"
+log "Output dir : ${OUTPUT_DIR}"
+num_images=$(find "${INPUT_DIR}" -type f -name '*.exr' | wc -l)
+log "Num images : ${num_images}"
+log "$SEPARATOR"
+
+log
+log "Training Parameters:"
+log "$SEPARATOR"
+log "Epochs     : ${NUM_EPOCHS}"
+log "Use patches: ${USE_PATCHES}"
+log "Num patches: ${NUM_PATCHES}"
+log "Patch size : ${PATCH_SIZE}"
+log "Interactive: ${INTERACTIVE}"
+log "App mode   : ${APP_MODE}"
+log "$SEPARATOR"
+
+CMD="uv run pht/models/afgsa/code/wo_diff_spec_decomp/train/train.py \
+  --inDir ${INPUT_DIR} \
+  --datasetDir ${OUTPUT_DIR}/patches \
+  --outDir ${OUTPUT_DIR} \
+  --epochs ${NUM_EPOCHS} \
+  --numPatches ${NUM_PATCHES} \
+  --patchSize ${PATCH_SIZE} \
+  --appMode ${APP_MODE}"
+
+if [[ ${USE_PATCHES} == false ]]; then
+    CMD+=" --useFullImage"
+fi
+if [[ ${APP_MODE} == "mamba" ]]; then
+    CMD+=" --lrD 1e-5 --lrG 1e-4 --ganLossW 1e-4 --gpLossW 1"
+fi
+
+log
+log "Training:"
+log "$SEPARATOR"
+log "Command: ${CMD}"
+
+START_TIME=$(date +%s)
+log "Start time: $(date -d "@${START_TIME}" '+%Y-%m-%d %H:%M:%S')"
+
+if [[ ${INTERACTIVE} == false ]]; then
+    export PYTHONUNBUFFERED=1
+    eval "${CMD}" 2>&1 | uv run tools/termlog.py "${OUTPUT_DIR}/train.txt"
+    export PYTHONUNBUFFERED=0
+else
+    eval "${CMD}"
+fi
+
+END_TIME=$(date +%s)
+ELAPSED=$((END_TIME - START_TIME))
+log "End time  : $(date -d "@${END_TIME}" '+%Y-%m-%d %H:%M:%S')"
+log "Elapsed   : ${ELAPSED} seconds"
+log "$SEPARATOR"
