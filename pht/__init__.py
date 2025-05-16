098411aee0c14f2a5da38389288209cd2dc08492
Manish Mathai <manishpmathai@gmail.com>
2025-05-16 16:03:40 -0700

    Lint top level python files

--- pht/__init__.py
+++ pht/__init__.py
@@ -1,5 +1,3 @@
-"""
-Pixel Heal Thyself - Deep learning based image denoiser for volumetric path traced images.
-"""
+"""Pixel Heal Thyself - Deep learning based image denoiser for volumetric path traced images."""
 
 __version__ = "0.1.0"
--- pht/utils.py
+++ pht/utils.py
@@ -1,26 +1,26 @@
+"""Utility functions."""
+
 import threading
-from typing import Any
+from typing import Any, ClassVar
 
 
-def is_truthy(value: Any) -> bool:
+def is_truthy(value: bool | str | int) -> bool:
     """Check if a value is truthy."""
-
     if isinstance(value, bool):
         return value
-    elif isinstance(value, str):
+    if isinstance(value, str):
         return value.lower() in ["true", "yes", "y"]
-    elif isinstance(value, int):
+    if isinstance(value, int):
         return value != 0
-    else:
-        raise ValueError(f"Invalid value: {value}")
+    return False
 
 
-def is_none(value: Any) -> bool:
+def is_none(value: bool | str | int) -> bool:
     """Check if a value is None."""
     return value is None or str(value).lower() == "none"
 
 
-def is_none_or_empty(value: Any) -> bool:
+def is_none_or_empty(value: bool | str | int) -> bool:
     """Check if a value is None or empty."""
     return is_none(value) or str(value) == ""
 
@@ -28,10 +28,11 @@ def is_none_or_empty(value: Any) -> bool:
 class SingletonMeta(type):
     """Metaclass for singleton classes."""
 
-    _instances = {}
+    _instances: ClassVar[dict[type, Any]] = {}
     _lock = threading.Lock()
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls, *args, **kwargs) -> Any:  # noqa: ANN002, ANN003, ANN401
+        """Create a new instance of the class if it doesn't exist. Otherwise, return the existing instance."""
         with cls._lock:
             if cls not in cls._instances:
                 cls._instances[cls] = super().__call__(*args, **kwargs)
